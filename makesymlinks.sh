f60def3bce846f710861cfbbbad18a883926c6cc
Alejandro Soto <asoto@boulder.swri.edu>
2019-09-09 11:42:47 -0600

    Fixed the code to install Vim plugins
    I fixed things so that the Vim plugins ended up in the right folder.
    
    	modified:   makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -41,27 +41,37 @@ rm $dir/vim/vim
 
 echo "Making sure I have all of the Vim plugins installed."
 # Now make sure I have all of the vim plugins that I use.
+if [ ! -d "$dir/vim/bundle" ]; then
+    mkdir "$dir/vim/bundle"
+fi
+
+bundle_dir="$dir/vim/bundle"
+
 # First check for the plugin manager Vundle. Git clone it
 # if I don't already have it.
 if [ ! -d "$dir/vim/bundle/Vundle.vim" ]; then
+    cd $bundle_dir
     git clone git@github.com:VundleVim/Vundle.vim.git
     echo "Vundle installed"
 fi
 
 # An indenting plugin.
 if [ ! -d "$dir/vim/bundle/indentpython.vim" ]; then
+    cd $bundle_dir
     git clone git@github.com:vim-scripts/indentpython.vim.git
     echo "Indent Python installed"
 fi
 
 # A plugin to check the syntax.
 if [ ! -d "$dir/vim/bundle/syntastic" ]; then
+    cd $bundle_dir
     git clone git@github.com:vim-syntastic/syntastic.git
     echo "Syntastic installed"
 fi
 
 # A plugin to check thet the Python code is PEP-8 compliant.
 if [ ! -d "$dir/vim/bundle/vim-flake8" ]; then
+    cd $bundle_dir
     git clone git@github.com:nvie/vim-flake8.git
     "vim-flake8 installed"
 fi
@@ -69,22 +79,26 @@ fi
 # A plugin that gives me a directory tree a la Atom or Sublime.
 # First the tree system itself.
 if [ ! -d "$dir/vim/bundle/nerdtree" ]; then
+    cd $bundle_dir
     git clone git@github.com:scrooloose/nerdtree.git
     echo "Nerdtree installed"
 fi
 # Then a way to work with tabs.
 if [ ! -d "$dir/vim/bundle/vim-nerdtree-tabs" ]; then
+    cd $bundle_dir
     git clone git@github.com:jistr/vim-nerdtree-tabs.git
 fi
 
 # A plugin to manage the info line at the bottom of the vim screen.
 # Plus a plugin to manage the themes used for that info line.
 if [ ! -d "$dir/vim/bundle/vim-airline" ]; then
+    cd $bundle_dir
     git clone git@github.com:vim-airline/vim-airline.git
     echo "vim-airline installed"
 fi
 
 if [ ! -d "$dir/vim/bundle/vim-airline-themes" ]; then
+    cd $bundle_dir
     git clone git@github.com:vim-airline/vim-airline-themes.git
     echo "vim-airline-themes installed"
 fi
